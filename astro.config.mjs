8dd432b0786683822ecfea01200337964b96fff5
MERLETTE <merlette@aliyun.com>
2025-09-17 11:06:18 +0800

    config: update sidebar navigation for programming guides

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,17 +9,13 @@ export default defineConfig({
 			title: 'TuringClub',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+   				{
+        			label: 'Guides',
+        			items: [
+            			{ label: 'Getting Started with Programming', slug: 'guides/programming/1' },           // 新增
+            			{ label: 'Java Programming Fundamentals', slug: 'guides/programming/2' },           // 新增
+        			],
+    			},
 			],
 		}),
 	],
